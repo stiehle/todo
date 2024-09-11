1ed368c194db6cfb809b8a2e082e2c24e6098cad
stiehle <gstiehle@yahoo.de>
2024-09-11 10:52:56 +0200

    Fix

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -81,8 +81,7 @@ export async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      // redirectTo: "http://localhost:5173/todo/",
-      redirectTo: "https://stiehle.github.io/todo",
+      redirectTo: "https://stiehle.github.io/todo/#",
     },
   });
   console.log(data, error);
